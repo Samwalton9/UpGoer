3e3474aa7de0d094b643b887abd3bb0a93bfe0d8
Samwalton9 <Samwalton9@gmail.com>
2022-11-08 15:06:35 +0000

    Add gravity, shuttle acceleration, and docked state

--- ShuttleView/Shuttle.gd
+++ ShuttleView/Shuttle.gd
@@ -1,24 +1,38 @@
 extends Sprite
 
-const SHUTTLE_ACCELERATION = 1
+const SHUTTLE_ACCELERATION = 15
 const SHUTTLE_MAX_SPEED = 55
 const RANDOM_INITIAL_ROTATION = 0.1
 const RANDOM_ROTATION_VARIATION = 0.3
 const ROTATION_CORRECTION_STRENGTH = 0.02
 
+const GRAVITY = Vector2(0, 10)
+
 var rotation_strength = 0.0
 var rotation_correction = 0.0
+
+var docked = true
+var original_position : Vector2
 
 
 func _ready() -> void:
 	rotation_degrees = rand_range(-RANDOM_INITIAL_ROTATION, RANDOM_INITIAL_ROTATION)
+	original_position = position
 
 
 func _physics_process(delta) -> void:
 	if Globals.flying:
-		position -= Vector2(0, SHUTTLE_MAX_SPEED).rotated(deg2rad(rotation_degrees)) * delta
+		var shuttle_engine_direction = Vector2(0, -Globals.shuttle_speed).rotated(deg2rad(rotation_degrees))
+		position += (shuttle_engine_direction + GRAVITY) * delta
 
-		# TODO: This is far from ideal, doesn't model things correctly once the shuttle is turning rapidly.
+		# Once the shuttle has overcome gravity, release from the dock.
+		if docked and -shuttle_engine_direction.y > GRAVITY.y:
+			docked = false
+
+		if docked:
+			# Shuttle is docked while it builds up acceleration
+			position.y = min(position.y, original_position.y)
+
 		rotation_degrees += rotation_strength + rotation_correction
 
 		# Update shuttle stats in use elsewhere
@@ -26,14 +40,10 @@ func _physics_process(delta) -> void:
 		Globals.shuttle_rotation_correction = rotation_correction
 		Globals.shuttle_rotation_strength = rotation_strength
 
-		# TODO: Temporary, this will accelerate eventually.
-		Globals.shuttle_speed = SHUTTLE_MAX_SPEED
+		Globals.shuttle_speed += SHUTTLE_ACCELERATION * delta
 
 		var input_strength = Input.get_axis("rotate_left", "rotate_right")
 
-		# This needs work - maybe input determines course correction strength, with the shuttle rotation varying randomly during the course of the game.
-		# Need to match the slider to the rotation strength that's occurring. It's constantly moving so you need to correct / match it
-		# on a regular basis.
 		rotation_correction += input_strength * ROTATION_CORRECTION_STRENGTH
 
 
