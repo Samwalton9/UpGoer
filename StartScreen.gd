9222261289c8b06469c0a993530ce4c9d495285a
Samwalton9 <Samwalton9@gmail.com>
2022-11-12 10:09:48 +0000

    Add click sounds to start menu

--- StartScreen.gd
+++ StartScreen.gd
@@ -1,6 +1,16 @@
 extends Control
 
+onready var clickDownAudio = $ClickDownAudio
+onready var clickUpAudio = $ClickUpAudio
 
 func _ready():
 	if Globals.restarted:
 		visible = false
+
+
+func _on_StartButton_button_down():
+	clickDownAudio.play()
+
+
+func _on_StartButton_button_up():
+	clickUpAudio.play()
