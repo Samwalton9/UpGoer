1e121ea77269988d992eabb64176f294ac86759b
Samwalton9 <Samwalton9@gmail.com>
2022-11-13 09:56:53 +0000

    Add preflightbutton click audio

--- ControlPanel/PreFlightButtons.gd
+++ ControlPanel/PreFlightButtons.gd
@@ -4,10 +4,16 @@ const NUM_PREFLIGHT_CHECKS = 3
 
 var correct_preflight_checks = 0
 
+onready var clickDownAudio = $ClickDownAudio
+onready var clickUpAudio = $ClickUpAudio
+
+
 func _ready():
 	var preflight_buttons = get_tree().get_nodes_in_group("PreFlightButtons")
 	for button in preflight_buttons:
 		button.connect("pressed", self, "_on_button_pressed", [button])
+		button.connect("button_down", self, "_on_button_down")
+		button.connect("button_up", self, "_on_button_up")
 
 
 	for _i in range(NUM_PREFLIGHT_CHECKS):
@@ -22,6 +28,7 @@ func _ready():
 func passed_preflight():
 	Globals.preflight_successful = true
 
+
 func _on_button_pressed(button):
 	var button_checks = get_tree().get_nodes_in_group("ButtonChecks")
 	if button_checks.has(button):
@@ -31,3 +38,11 @@ func _on_button_pressed(button):
 
 		if correct_preflight_checks == 3:
 			passed_preflight()
+
+
+func _on_button_down():
+	clickDownAudio.play()
+
+
+func _on_button_up():
+	clickUpAudio.play()
