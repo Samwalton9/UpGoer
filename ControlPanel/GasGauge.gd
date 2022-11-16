924fa4bb85fd10b6d4c8dc8b23f2355d023258d2
Sam Walton <samwalton9@gmail.com>
2022-11-16 09:48:00 +0000

    Simplify to one horizontal gas gauge

--- ControlPanel/GasGauge.gd
+++ ControlPanel/GasGauge.gd
@@ -3,27 +3,22 @@ extends Control
 onready var gauge = $GaugeLevel
 onready var gauge_fill_timer = $Timer
 
-var max_height
-var original_position
+var max_width
 var random_growth_speed
 var level = 0
+var action_visible = false
 
-export var gauge_input : String = ""
 export var gauge_color : Color
 
 func _ready():
 
-	max_height = gauge.rect_size.y
-	original_position = gauge.rect_position.y
-	random_growth_speed = get_random_growth_speed()
+	max_width = gauge.rect_size.x
+
+	# Deliberate start speed so gauge is introduced at the right time
+	random_growth_speed = 0.15
 
 	# Initialise empty
-	gauge.rect_size.y = 0
-	gauge.rect_position.y += gauge.rect_size.y
-
-	for action in InputMap.get_action_list(gauge_input):
-		# Assume there's only one possible, physical key, input
-		$KeyBox/Key.text = OS.get_scancode_string(action.physical_scancode)
+	gauge.rect_size.x = 0
 
 	gauge.color = gauge_color
 
@@ -32,14 +27,16 @@ func _process(delta):
 
 	if Globals.flying:
 		# On every reset, change growth speeds
-		if Input.is_action_just_pressed(gauge_input):
+		if Input.is_action_just_pressed("gauge_reset"):
 			level = 0
 			random_growth_speed = get_random_growth_speed()
 			gauge_fill_timer.stop()
+			action_visible = false
+			$KeyBox.color = Color(0.5,0.5,0,1)
+			$KeyBox/Key.text = ""
 
-		var fractional_height = (level * max_height)
-		gauge.rect_size.y = fractional_height
-		gauge.rect_position.y = original_position + max_height - fractional_height
+		var fractional_width = (level * max_width)
+		gauge.rect_size.x = fractional_width
 
 		var growth_amount = random_growth_speed * delta
 
@@ -55,11 +52,20 @@ func _process(delta):
 			if gauge_fill_timer.is_stopped():
 				gauge_fill_timer.start()
 
+		if not action_visible and level > 0.5:
+			set_action_visible()
+
 		level += growth_amount
 
 
 func get_random_growth_speed() -> float:
-	return rand_range(0.05,0.2)
+	return rand_range(0.1,0.3)
+
+
+func set_action_visible():
+	action_visible = true
+	$KeyBox.color = Color(1,1,0,1)
+	$KeyBox/Key.text = "Space"
 
 
 func _on_Timer_timeout():
